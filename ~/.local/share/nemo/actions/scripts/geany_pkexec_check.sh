aae6b970f62cd3f69b85bb69d6de5ffae08d26cf
steve <29017677+smurphos@users.noreply.github.com>
2019-04-13 08:44:19 +0100

    Remove actions requiring polkit polocy

--- ~/.local/share/nemo/actions/scripts/geany_pkexec_check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/dash
-if ls -l "$1" | grep -q $USER; then
-	geany "$1"
-else
-	pkexec geany "$1"
-fi
--- ~/.local/share/nemo/actions/scripts/xed_pkexec_check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/dash
-if ls -l "$1" | grep -q $USER; then
-	xed "$1"
-else
-	pkexec xed "$1"
-fi
