4899c77c82967a0d3657bbf260291dd4a873bbb8
steve <29017677+smurphos@users.noreply.github.com>
2019-10-19 09:31:38 +0100

    Disk Usage action - symbolic link support

--- .local/share/nemo/actions/scripts/disk_usage.sh
+++ .local/share/nemo/actions/scripts/disk_usage.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zenity --info --no-wrap --title="Disk Usage" --text="Combined Disk Usage of\n\n$(printf '%s\n' "$@")\n\n$(du -hc "$@" | tail -n1 | cut -d '	' -f 1)"
+zenity --info --no-wrap --title="Disk Usage" --text="Combined Disk Usage of\n\n$(printf '%s\n' "$@")\n\n$(du -Lhc "$@" | tail -n1 | cut -d '	' -f 1)"
