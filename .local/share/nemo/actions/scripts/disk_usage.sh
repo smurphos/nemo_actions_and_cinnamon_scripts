1eb1f4abc4084b05947a16d6ab72244a3b6a39ea
steve <29017677+smurphos@users.noreply.github.com>
2019-10-20 06:20:51 +0100

    Disk Usage - make following symlinks optional based on initial selection

--- .local/share/nemo/actions/scripts/disk_usage.sh
+++ .local/share/nemo/actions/scripts/disk_usage.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-zenity --info --no-wrap --title="Disk Usage" --text="Combined Disk Usage of\n\n$(printf '%s\n' "$@")\n\n$(du -Lhc "$@" | tail -n1 | cut -d '	' -f 1)"
+for DIR in "$@"; do
+  if [ -L "$DIR" ]; then
+    if zenity --question --icon-name=dialog-question --no-wrap --text="One of your selections is a symlink.\n\nDo you want to include the disk usage of symlinked files and directories?"; then
+      zenity --info --no-wrap --title="Disk Usage (Inc. Symlinks)" --text="Combined Disk Usage of\n\n$(printf '%s\n' "$@")\n\n$(du -Lhc "$@" | tail -n1 | cut -d '	' -f 1)"
+      exit
+    else
+      break
+    fi
+  fi
+done
+zenity --info --no-wrap --title="Disk Usage" --text="Combined Disk Usage of\n\n$(printf '%s\n' "$@")\n\n$(du -hc "$@" | tail -n1 | cut -d '	' -f 1)"
+
+
